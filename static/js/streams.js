9e5802ce6713ae5a2dd85678b6d80c234591d1e3
callmearya <60613987+callmearya@users.noreply.github.com>
2024-07-16 12:29:11 +0530

    Update streams.js

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,5 +1,5 @@
 
-const APP_ID = 'YOUR APP ID'
+const APP_ID = '2bf27cce012b4d67a80beb64772c595d'
 const TOKEN = sessionStorage.getItem('token')
 const CHANNEL = sessionStorage.getItem('room')
 let UID = sessionStorage.getItem('UID')
